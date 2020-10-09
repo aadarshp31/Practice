3c51210bdb405e05fea6ce4c754fb26004b71180
aadarshp31 <adarshpandey.in@gmail.com>
2020-10-09 17:20:20 +0530

    Added my 1st dotNet MVC project.

---
+++ DotNet/WebApplication2/Controllers/MyFirstController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class MyFirstController : Controller
+    {
+        // GET: MyFirst
+        //The functions/methods below Called as Actions not methods
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Apple()
+        {
+            return "<input type='text' value='Ooty Apple' />";
+        }
+
+        public string Orange()
+        {
+            return "<h3>Nagpur Orange</h3>";
+        }
+
+        // [HttpGet]
+        public string Prunes(int id)
+        {
+            return $"val {id}";
+        }
+
+        public string Jack()
+        {
+            string str = "<form method='post' action='jack/88' ><input type='submit' value='Make a Post Request' /></form>";
+            return $"{str}";
+        }
+
+        [HttpPost]
+        public string Jack(int id)
+        {
+            return $"Jack = {id}";
+        }
+
+        public ViewResult Mango()
+        {
+            return View();
+        }
+    }
+}
